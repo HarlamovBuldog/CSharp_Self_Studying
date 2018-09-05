4adbd2c3c07a691e2ccc68363131649830eb2d01
HarlamovBuldog <vladwoode@gmail.com>
2018-09-05 16:42:58 +0300

    Another rather big project with example of working with db powered by MVVM pattern added

---
+++ WPF/MVVM_pattern/wpf_EntityFramework/wpf_EntityFramework/ViewModels/RowVM/CustomerVM.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Support;
+namespace wpf_EntityFramework
+{
+    public class CustomerVM : VMBase
+    {
+        public Customers TheCustomer { get; set; }
+        public CustomerVM()
+        {
+            // Initialise the entity or inserts will fail
+            TheCustomer = new Customers();
+            TheCustomer.CreditLimit = 5000;
+            TheCustomer.Outstanding = 0;
+        }
+
+    }
+}
